85ffc7be81adbfd0599d3941b949e13434a0b738
litreily <707922098@qq.com>
2018-02-08 09:49:07 -0500

    complete simple capturer features from sina
    as for now, must provide user id and cookie of the browser
    it will capture imgs from url like below:
    http://weibo.cn/album/albummblog/?rl=11&fuid=2657006573&page=1
    
    Signed-off-by: litreily <707922098@qq.com>

--- capturer.py
+++ capturer.py
@@ -11,34 +11,93 @@ import os
 webType = 'default'
 encodeType = {'default':'UTF-8', 'sina':'gb2312'}
 
-def getHtml(url):
+def getStaticHtml(url):
     global webType
     global encodeType
 
     page = urllib.request.urlopen(url)
     html = page.read().decode(encodeType[webType])
     return html
-     
+
+def getDynamicHtml(url, headers):
+    global webType
+    global encodeType
+
+    req = urllib.request.Request(url, headers = headers)
+    page = urllib.request.urlopen(req)
+    html = page.read().decode(encodeType[webType])
+    return html
+
 def saveHtml(html):
     txt='result.html'  
     f = open(txt,"w+")  
     f.write(html)
 
-def saveImg(html): 
+def saveImgFromStaticWeb(html, path): 
     reg = r'src="(.+?\.jpg)"'
     imgre = re.compile(reg)
-    imglist = imgre.findall(html)
+    imgUrls = imgre.findall(html)
     x = 0
-    path = '/home/litreily/capturer.git/download'
     if not os.path.isdir(path):
         os.makedirs(path)
     paths = path+'/'
-    for imgurl in imglist:
-        urllib.request.urlretrieve(imgurl,'{}{}.jpg'.format(paths,x))
+
+    for imgUrl in imgUrls:
         x = x + 1
+        urllib.request.urlretrieve(imgUrl,'{}{}.jpg'.format(paths,x))
+        print("%d.jpg" % (x))
                                                                
+def saveImgFromSina(uid, dir):
+    count = 0
+    page = 1
+
+    print('start capture picture of uid:' + str(uid))
+    while True:
+        reg = r'src="http://w(.{2})\.sinaimg.cn/(wap180|square)/(.{32,33}.(jpg|gif))" alt'
+        imgre = re.compile(reg)
+        url = r'http://weibo.cn/album/albummblog/?rl=11&fuid='+ str(uid) + '&page='+ str(page)
+        # cookie is form the above url->network->request headers
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+                   'Cookie': 'YOUR COOKIES IN THE ABOVE URL'}
+        html = getDynamicHtml(url, headers)
+        saveHtml(html)
+        imgIds = imgre.findall(html)
+        if len(imgIds) == 0:
+            print("capture complete!")
+            break;
+
+        imgUrls = []
+        for id in imgIds:
+            imgUrl = 'http://w' + id[0] + '.sinaimg.cn/large/' + id[2]
+            imgUrls.append(imgUrl)
+        imgUrls.reverse()
+
+        path = str(dir) + '/' + str(uid)
+        if not os.path.isdir(path):
+            os.makedirs(path)
+        paths = path+'/'
+        
+        print('page '+ str(page))
+        for imgUrl in imgUrls:
+            count += 1
+            # display the raw url of images
+            print('%d\t%s' % (count, imgUrl))
+            urllib.request.urlretrieve(imgUrl, '{}{}.{}'.format(paths, count, imgUrl[-3:]))
+        page += 1
+        print('')
+
+
+dir = '/home/litreily/web/www/html/images'
+
+# capture imgs from static web page
+'''
 url = r"http://blog.csdn.net/ben_ben_niao/article/details/40677869"
-#url = r"https://weibo.com/p/1005052657006573/photos?from=page_100505&mod=TAB#place"
-html = getHtml(url)
+html = getStaticHtml(url)
 saveHtml(html)
-saveImg(html)
+saveImgFromStaticWeb(html,dir + '/static')
+'''
+
+# capture imgs from sina
+uids = ['2173752092', '2657006573']
+uid = uids[1]
+saveImgFromSina(uid, dir)
